123f707deb141513c8c7171b541da652243e6b7d
AngryBacteria <AngryBacteria@protonmail.com>
2024-11-13 17:55:42 +0100

    initv2

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ NumberSystem = Literal["hex", "dec", "bin", "oct"]
 
 
 def convert_number(
-        number: Union[str, int], from_system: NumberSystem, to_system: NumberSystem
+    number: Union[str, int],
+    from_system: Literal["hex", "dec", "bin", "oct"],
+    to_system: NumberSystem,
 ) -> str:
     if isinstance(number, str):
         number = number.replace(" ", "")
@@ -38,10 +40,12 @@ def convert_number(
         raise ValueError("Invalid 'to' number system")
 
 
+# Calculate the number of possibilities for a password of a given length and number of unique characters
 def password_possibilities(length: int, unique_characters: int) -> int:
-    return unique_characters ** length
+    return unique_characters**length
 
 
+# Calculate the entropy of a password with a given length and number of unique characters
 def password_entropy(length: int, unique_characters: int, ceil_value=True) -> float:
     value = length * log2(unique_characters)
     if ceil_value:
@@ -49,6 +53,7 @@ def password_entropy(length: int, unique_characters: int, ceil_value=True) -> fl
     return value
 
 
+# Checks if a number is valid by the luhn algorithm
 def is_valid_luhn(number: str | int) -> bool:
     # Remove any spaces or hyphens
     number = str(number)
@@ -74,6 +79,7 @@ def is_valid_luhn(number: str | int) -> bool:
     return total % 10 == 0
 
 
+# Checks if a digit is valid by the luhn algorithm
 def generate_check_digit(partial_number: str | int) -> str:
     partial_number = str(partial_number)
     for check_digit in range(10):
@@ -81,6 +87,7 @@ def generate_check_digit(partial_number: str | int) -> str:
             return partial_number + str(check_digit)
 
 
+# Generates numbers by using a linear congruential generator
 def linear_congruential_generator(init_x: int, a=6, b=0, m=13, n=1):
     output = []
     x = init_x
@@ -90,15 +97,16 @@ def linear_congruential_generator(init_x: int, a=6, b=0, m=13, n=1):
     return output
 
 
-# TODO: understand
 def decrypt_xor(ciphertext: bytes, key: int):
     return "".join(chr(c ^ key) for c in ciphertext)
 
 
+# Brute force a cipher text by trying all different keys in a range from 0 to 256
+# Uses a hex string as input
 def brute_force_xor_cypher(
-        cypher_text="f7 d0 d8 d1 cc d3 df ca d7 d1 d0 cd cd d7 dd d6 db cc d6 db d7 ca 9e fc ea e6 86 8e 88 8b 9e "
-                    "8c 8c 91 8c 8d",
-        filter_non_ascii=True,
+    cypher_text="f7 d0 d8 d1 cc d3 df ca d7 d1 d0 cd cd d7 dd d6 db cc d6 db d7 ca 9e fc ea e6 86 8e 88 8b 9e "
+    "8c 8c 91 8c 8d",
+    filter_non_ascii=True,
 ):
     # Hexadezimal-String in Bytes umwandeln
     ciphertext = bytes.fromhex(cypher_text)
@@ -108,13 +116,14 @@ def brute_force_xor_cypher(
         if filter_non_ascii:
             plaintext = decrypt_xor(ciphertext, key)
             if all(
-                    32 <= ord(c) <= 126 or ord(c) in [10, 13] for c in plaintext
+                32 <= ord(c) <= 126 or ord(c) in [10, 13] for c in plaintext
             ):  # Prüfen auf druckbare ASCII-Zeichen
                 print(f"Schlüssel {key}: {plaintext}")
         else:
             print(f"Schlüssel {key}: {decrypt_xor(ciphertext, key)}")
 
 
+# Does execute xor_bitwise computation on two integers (either decimal or binary)
 def xor_bitwise(a: int, b: int, as_binary=False):
     if as_binary:
         return bin(a ^ b)
@@ -122,15 +131,17 @@ def xor_bitwise(a: int, b: int, as_binary=False):
         return a ^ b
 
 
+# Helper function to check if a number is prime or not
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
 
+# Generates a prime number in a given range
 def generate_prime(min_val, max_val):
     prime = random.randrange(min_val, max_val)
     while not is_prime(prime):
@@ -138,6 +149,7 @@ def generate_prime(min_val, max_val):
     return prime
 
 
+# Mod inverse using the extended Euclidean algorithm
 def mod_inverse(e, phi):
     def extended_gcd(a, b):
         if a == 0:
@@ -151,6 +163,7 @@ def mod_inverse(e, phi):
     return (x % phi + phi) % phi
 
 
+# Generates a RSA keypair
 def generate_keypair(p=generate_prime(10, 1000), q=generate_prime(10, 1000), e=0):
     n = p * q
     phi = (p - 1) * (q - 1)
